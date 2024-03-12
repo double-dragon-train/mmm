020a51500f058ef754cf61087a210588ed355fed
한규준 <>
2024-03-12 12:31:26 +0900

    add: 예외처리 코드 작성

---
+++ backend/src/main/java/com/spring/mmm/common/exception/GlobalErrorExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.spring.mmm.common.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalErrorExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(CustomException e, HttpServletRequest request){
+        log.debug("Custom Exception Occur", e);
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus())
+                .body(ErrorResponse.createErrorResponse(e.getErrorCode(), request.getRequestURI()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerException(Exception e, HttpServletRequest request){
+        log.warn("Unhandled Exception 발생!!", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.createErrorResponse(GlobalErrorCode.INTERNAL_SERVER_ERROR_CODE, request.getRequestURI()));
+    }
+}
