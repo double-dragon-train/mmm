36aa5fe21cb7fb0f68d3e4ac90aa3912797140b9
한규준 <>
2024-03-15 10:54:46 +0900

    feat: 에러코드 추가

--- backend/src/main/java/com/spring/mmm/common/exception/GlobalErrorCode.java
+++ backend/src/main/java/com/spring/mmm/common/exception/GlobalErrorCode.java
@@ -14,7 +14,7 @@ public enum GlobalErrorCode implements ErrorCode {
     LOCAL_FILE_UPLOAD_FAILED(
             HttpStatus.INTERNAL_SERVER_ERROR,
             "잠시후 시도해주세요. 계속해서 문제가 발생한 경우 관리자 이메일(goumunity@gmail.com)로 연락주세요."),
-    FILE_IS_NOT_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "전송한 파일이 이미지 타입이 아닙니다."),
+    FILE_TYPE_ERROR(HttpStatus.BAD_REQUEST, "파일이 이미지 타입이 아닙니다."),
 
     INTERNAL_SERVER_ERROR_CODE(
             HttpStatus.INTERNAL_SERVER_ERROR,
