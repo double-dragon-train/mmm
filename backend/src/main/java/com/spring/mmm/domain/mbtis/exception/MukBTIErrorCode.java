423e6946431647ef756e621985562fcdec0e7722
한규준 <>
2024-03-24 21:39:00 +0900

    feat: 에러코드 추가

--- backend/src/main/java/com/spring/mmm/domain/mbtis/exception/MukBTIErrorCode.java
+++ backend/src/main/java/com/spring/mmm/domain/mbtis/exception/MukBTIErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum MukBTIErrorCode implements ErrorCode {
-    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 MukBTI를 발견하지 못했습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 MukBTI를 발견하지 못했습니다."),
+    NOT_FOUND_QUESTION(HttpStatus.NOT_FOUND, "해당 질문을 발견하지 못했습니다."),
+    NOT_FOUND_ANSWER(HttpStatus.NOT_FOUND, "해당 답변을 발견하지 못했습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
