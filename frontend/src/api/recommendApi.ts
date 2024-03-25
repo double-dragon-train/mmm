87a5a2513d6d910d2ba544a2c8592136af1287a1
wkd940709 <wkd940709@naver.com>
2024-03-25 14:12:23 +0900

    feat: 먹기록 모달 디자인 구현

--- frontend/src/api/recommendApi.ts
+++ frontend/src/api/recommendApi.ts
@@ -9,3 +9,13 @@ export async function getRandomFoodList() {
     console.log(e);
   }
 }
+
+export async function getRecentRecommendFood() {
+  try {
+    const res = await instance.get('/mukus/groups/0/recent');
+    // const res = await axios.get(`${VITE_API_DEV}/recommend`)
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
