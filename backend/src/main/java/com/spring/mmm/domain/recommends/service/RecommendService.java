6d44b648a1198379788bf2dcc0dcf7d461d8f84f
한규준 <>
2024-03-27 14:25:11 +0900

    feat: 점메추 기능 작성

--- backend/src/main/java/com/spring/mmm/domain/recommends/service/RecommendService.java
+++ backend/src/main/java/com/spring/mmm/domain/recommends/service/RecommendService.java
@@ -1,9 +1,13 @@
 package com.spring.mmm.domain.recommends.service;
 
+import com.spring.mmm.domain.recommends.controller.request.LunchRecommendRequest;
 import com.spring.mmm.domain.recommends.controller.response.FoodInformation;
+import com.spring.mmm.domain.recommends.controller.response.LunchRecommendFoodInformation;
 
 import java.util.List;
 
 public interface RecommendService {
     List<FoodInformation> recommendRandomFood();
+
+    List<LunchRecommendFoodInformation> lunchRecommendFood(LunchRecommendRequest lunchRecommendRequest);
 }
