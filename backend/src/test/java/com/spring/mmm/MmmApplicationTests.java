4cbd72dcd1bc0d41e5020775e2a20d51668bb4d7
김규현 <kgh2120@gmail.com>
2024-04-01 17:21:58 +0900

    chore: SBT Profile 수정

--- backend/src/test/java/com/spring/mmm/MmmApplicationTests.java
+++ backend/src/test/java/com/spring/mmm/MmmApplicationTests.java
@@ -3,8 +3,10 @@ package com.spring.mmm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile("dev")
 @SpringBootTest
 class MmmApplicationTests {
 
