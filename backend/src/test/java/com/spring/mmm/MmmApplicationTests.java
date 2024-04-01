a522cb038c7a279030208a2be8312c22aa240118
김규현 <kgh2120@gmail.com>
2024-04-01 17:30:50 +0900

    chore: SBT ActiveProfiles 수정

--- backend/src/test/java/com/spring/mmm/MmmApplicationTests.java
+++ backend/src/test/java/com/spring/mmm/MmmApplicationTests.java
@@ -3,12 +3,15 @@ package com.spring.mmm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
-@Profile("dev")
+@ActiveProfiles("dev")
 @SpringBootTest
 class MmmApplicationTests {
+
+    @Autowired
+    Environment env;
 
     @Test
     void contextLoads() {
