8c4d34f2d153f6edbb3fe776b3a8614d05f77edb
김규현 <kgh2120@gmail.com>
2024-04-01 14:54:03 +0900

    fix: MukgroupResponse에 타이틀 먹적 정보(이름,이미지소스) 추가

--- backend/src/main/java/com/spring/mmm/domain/mukgroups/controller/response/MukgroupResponse.java
+++ backend/src/main/java/com/spring/mmm/domain/mukgroups/controller/response/MukgroupResponse.java
@@ -1,25 +1,40 @@
 package com.spring.mmm.domain.mukgroups.controller.response;
 
+import org.springframework.util.StringUtils;
+
 import com.spring.mmm.domain.mukgroups.domain.MukgroupEntity;
-import lombok.*;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class MukgroupResponse {
-    private Long mukgroupId;
-    private String name;
-    private Boolean isSolo;
-    private String imageSrc;
-    private Long titleMukjukId;
+	private Long mukgroupId;
+	private String name;
+	private Boolean isSolo;
+	private String imageSrc;
+	private String titleMukjukName;
+	private String titleMukjukImage;
 
-    public static MukgroupResponse createByMukgroupEntity(MukgroupEntity mukgroupEntity){
-        return MukgroupResponse.builder()
-                .mukgroupId(mukgroupEntity.getMukgroupId())
-                .name(mukgroupEntity.getName())
-                .isSolo(mukgroupEntity.getIsSolo())
-                .imageSrc(mukgroupEntity.getImageSrc())
-                .build();
-    }
+	public static MukgroupResponse createByMukgroupEntity(MukgroupEntity mukgroupEntity) {
+
+
+
+		return MukgroupResponse.builder()
+			.mukgroupId(mukgroupEntity.getMukgroupId())
+			.name(mukgroupEntity.getName())
+			.isSolo(mukgroupEntity.getIsSolo())
+			.imageSrc(mukgroupEntity.getImageSrc())
+			.titleMukjukImage(mukgroupEntity.getMukjukEntity() != null
+				? mukgroupEntity.getMukjukEntity().getImageSrc() : null)
+			.titleMukjukName(mukgroupEntity.getMukjukEntity() != null
+				? mukgroupEntity.getMukjukEntity().getName() : null)
+			.build();
+	}
 }
